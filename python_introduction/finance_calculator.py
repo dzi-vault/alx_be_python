bdfcb9470f3646388824abc426b8b43a79d00fb3
Tina Tettey <157775692+dzi-vault@users.noreply.github.com>
2025-11-09 20:44:01 +0000

    finance calculator update

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
 
 monthly_income = int(input("Enter your monthly income:"))
 
-total_monthly_expenses = int(input("Enter your total monthly expenses:"))
+monthly_expenses = int(input("Enter your total monthly expenses:"))
 
-monthly_savings = monthly_income - total_monthly_expenses
+monthly_savings = monthly_income - monthly_expenses
 
 simple_annual_interest_rate = 0.05
 
