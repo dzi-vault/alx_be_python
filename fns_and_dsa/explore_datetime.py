9a69443a50d7c499c48139fe504a3c20e355ca0e
Tina Tettey <157775692+dzi-vault@users.noreply.github.com>
2025-11-23 17:27:48 +0000

    current datetime & calculate future date

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(formatted_date)
+display_current_datetime()
+
+
+from datetime import datetime, timedelta
+
+def calculate_future_date():
+    days_to_add = int(input("Enter the number of days to add to the current date: "))
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=days_to_add)
+    print(f"Future date: {future_date.strftime('%Y-%m-%d')}")
+
+# Call the function
+calculate_future_date()
