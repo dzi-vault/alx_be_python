f08f4c4e515248f83b34c15991a11e167a77965d
Tina Tettey <157775692+dzi-vault@users.noreply.github.com>
2025-11-30 18:49:26 +0000

    Write Unit Tests for a Simple Calculator Class Update2

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -14,10 +14,10 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(0, 5), -5)
 
-    def test_multiplication(self):  # <-- Correct name per checker
+    def test_multiplication(self):  # Correct per checker
         self.assertEqual(self.calc.multiply(4, 3), 12)
         self.assertEqual(self.calc.multiply(-1, 5), -5)
 
-    def test_divide(self):  # This name is correct
+    def test_division(self):  # Changed from test_divide to test_division
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(10, 0), None)
